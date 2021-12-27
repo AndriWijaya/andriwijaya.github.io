0493a927cf2da5fcdf1dc314afe98f9198269e25
wijayakusuma1972 <wijayakusuma1972@gmail.com>
2021-12-27 22:44:52 +0700

    Updates

---
+++ precache-manifest.edfb65fa291b45f13886eee707db82a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59697a16f8f897c617db",
+    "url": "/static/css/main.843cd711.chunk.css"
+  },
+  {
+    "revision": "59697a16f8f897c617db",
+    "url": "/static/js/main.0b245666.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2350d3a5bcc87ba2d2ed",
+    "url": "/static/js/2.5d2b889c.chunk.js"
+  },
+  {
+    "revision": "e2ad74c8ad9b9f328bf12d0dc7fa01fd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/poke-mon/precache-manifest.bf94e08c28e11b7661bcc91a5ebae970.js"
+  "/precache-manifest.edfb65fa291b45f13886eee707db82a4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/poke-mon/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
