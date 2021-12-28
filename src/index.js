531640eafeb0afecd9f906f3ff969297cf078962
wijayakusuma1972 <wijayakusuma1972@gmail.com>
2021-12-28 17:01:13 +0700

    route 2

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as conn from './connection.js'; 
 import { ApolloProvider } from "@apollo/client";
-import { Route, NavLink, Routes, HashRouter } from "react-router-dom";
+import { Route, NavLink, Routes, BrowserRouter } from "react-router-dom";
 import AllPokemon from './page/AllPokemon';
 import Detail from './page/Detail';
 import MyPokemon from './page/MyPokemon';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <NavLink exact="true" to="/" className="navbar-brand">Wild Nature</NavLink>
@@ -20,10 +20,10 @@ function App() {
       
       <Routes>
         <Route path="/" element={ <AllPokemon/> }/>
-        <Route path="/my-pokemon" element={ <MyPokemon/> }/>
-        <Route path="/detail/:name" element={ <Detail/> }/>
+        <Route path="my-pokemon" element={ <MyPokemon/> }/>
+        <Route path="detail/:name" element={ <Detail/> }/>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
