61351c4622e1d6b0778c66fe1581c48f0a1bfee2
wijayakusuma1972 <wijayakusuma1972@gmail.com>
2021-12-28 17:03:09 +0700

    Updates

---
+++ precache-manifest.7b28f353ea5d5df2e403dcfad3e87c34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c32cd12d9bb2ee7c32e",
+    "url": "/static/css/main.aa76c76b.chunk.css"
+  },
+  {
+    "revision": "7c32cd12d9bb2ee7c32e",
+    "url": "/static/js/main.7c32cd12.chunk.js"
+  },
+  {
+    "revision": "6f3e3402a7bdbcf93ed6",
+    "url": "/static/js/1.6f3e3402.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d258df8df14f6d2ba958c80bc6d1f7f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19a32419a6b9e5726143fe09856e9ef5.js"
+  "/precache-manifest.7b28f353ea5d5df2e403dcfad3e87c34.js"
 );
 
 workbox.clientsClaim();
